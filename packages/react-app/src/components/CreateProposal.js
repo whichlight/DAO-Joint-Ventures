f152c746d60e1959f5171cb6f2a9ae8797a9ed18
Noman <noman@noman.land>
2022-09-17 15:08:57 -0400

    Deploy scaffolding

--- packages/react-app/src/components/CreateProposal.js
+++ packages/react-app/src/components/CreateProposal.js
@@ -1,19 +1,48 @@
+import { Contract, ContractFactory } from '@ethersproject/contracts';
+import { useEthers } from '@usedapp/core';
+
 import { useDaoInfo, useNewTokenInfo } from '../hooks/infoHooks';
 import { DaoInfo } from './DaoInfo';
 import { NewTokenInfo } from './NewTokenInfo';
 
+import { abi } from '../contracts/abi';
+import { bytecode } from '../contracts/bytecode';
+import { useCallback, useEffect, useState } from 'react';
+
 export const CreateProposal = () => {
-  // Read more about useDapp on https://usedapp.io/
-  // const { error: contractCallError, value: tokenBalance } =
-  //   useCall({
-  //     contract: new Contract(addresses.ceaErc20, abis.erc20),
-  //     method: 'balanceOf',
-  //     args: ['0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C'],
-  //   }) ?? {};
+  const [contractAddress, setContractAddress] = useState('');
+  const { activate, library } = useEthers();
+
+  // useEffect(() => activate(), [activate]);
 
   const daoInfo1 = useDaoInfo();
   const daoInfo2 = useDaoInfo();
   const tokenInfo = useNewTokenInfo();
+
+  const deploy = useCallback(async () => {
+    const contractFactory = new ContractFactory(
+      abi,
+      bytecode,
+      library.getSigner()
+    );
+
+    contractFactory
+      .deploy(
+        daoInfo1.daoAddress,
+        daoInfo1.numTokens,
+        daoInfo2.daoAddress,
+        daoInfo2.numTokens,
+        daoInfo1.tokenAddress,
+        daoInfo2.tokenAddress,
+        daoInfo1.split1,
+        tokenInfo.tokenName,
+        tokenInfo.tokenSymbol,
+        111111
+      )
+      .then(contract => {
+        setContractAddress(contract.address);
+      });
+  }, [daoInfo1, daoInfo2, library, tokenInfo]);
 
   const isValid =
     daoInfo1.daoAddress &&
@@ -32,7 +61,14 @@ export const CreateProposal = () => {
       <DaoInfo index={0} daoInfo={daoInfo1} />
       <DaoInfo index={1} daoInfo={daoInfo2} />
       <NewTokenInfo tokenInfo={tokenInfo} />
-      <button disabled={!isValid}>Deploy</button>
+
+      {!contractAddress ? (
+        <button disabled={!isValid} onClick={deploy}>
+          Deploy
+        </button>
+      ) : (
+        <div>Deployed!: {contractAddress}</div>
+      )}
     </div>
   );
 };
